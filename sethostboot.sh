598145e928abcada46a61653021d83b283f12c5d
salperes <salper.es@gmail.com>
2020-03-26 18:14:30 +0300

    set hostname and boot/config.txt parameters

---
+++ sethostboot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+MAC=`hostname`
+if [ $MAC == "raspberrypi" ]
+ then
+        MAC='coord-'
+        MAC=$MAC`cat /sys/class/net/eth0/address | tr -d : | tr '[:lower:]' '[:upper:]'`
+        echo $MAC
+
+        hostname $MAC
+        echo $MAC > /etc/hostname
+
+        sed 's/raspberrypi/'$MAC'/' /etc/hosts > /etc/hosts_
+        mv /etc/hosts_ /etc/hosts
+
+	echo 'dtoverlay=gpio-no-irq' >> /boot/config.txt
+	sed 's/dtparam=audio=on/dtparam=audio=off/' /boot/config.txt > /boot/config.txt_
+	mv /boot/config.txt_ /boot/config.txt
+
+        reboot
+        exit
+
+else
+        exit
+fi
+
